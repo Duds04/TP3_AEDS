d24d6fef3a2592d5f18ea5a004a8cae9469c86d2
João Bradachi <105111795+JBradachi@users.noreply.github.com>
2022-10-07 14:30:52 -0300

    Add files via upload

--- ListaPalavras.c
+++ ListaPalavras.c
@@ -1,17 +1,19 @@
 #include "ListaPalavras.h"
 
-int Inicia_Lista(TLista_de_palavras *pLista){
+/*  Implementação com célula cabeça em todas funções   */
 
-    pLista->pPrimeiro = (Apontador) malloc(sizeof(TCelula));
+int LP_Inicia_Lista(TLista_de_palavras *pLista){
+
+    pLista->pPrimeiro = (Apontador_celula) malloc(sizeof(TCelula));
     pLista->pUltimo = pLista->pPrimeiro;
     pLista->pUltimo->pProx = NULL;
 
     return 1;
 }
 
-int Insere_no_final(TLista_de_palavras *pLista/*, Tipo_palavra palavra*/){
+int LP_Insere_no_final(TLista_de_palavras *pLista/*, Tipo_palavra palavra*/){
 
-    pLista->pUltimo->pProx = (Apontador) malloc(sizeof(TCelula));
+    pLista->pUltimo->pProx = (Apontador_celula) malloc(sizeof(TCelula));
     pLista->pUltimo = pLista->pUltimo->pProx;
     /*pLista->pUltimo->TAD da palavra = palavra*/
     pLista->pUltimo->pProx = NULL;
@@ -19,11 +21,33 @@ int Insere_no_final(TLista_de_palavras *pLista/*, Tipo_palavra palavra*/){
     return 1;
 }
 
+int LP_Numero_de_palavras(TLista_de_palavras *pLista){
+
+    int contador = 0;
+    Apontador_celula pAux;
+
+    pAux = pLista->pPrimeiro->pProx;
+    while (pAux != NULL){
+        contador++;                 /*  Esta função percorre toda a lista somando +1 no contador até o ultimo item   */
+        pAux = pAux->pProx;
+    }
+    return contador;
+}
+
+int LP_Verifica_palavra(TLista_de_palavras *pLista/*, TAD_palavra *ponteiro da palavra*/){
+    Apontador_celula pAux;
+    pAux = pLista->pPrimeiro->pProx;
+    while (pAux != NULL){
+        /*  if(pAux->""ITEM"" == ponteiro_da_palavra->""ITEM"")
+            return 1;   */
+        pAux = pAux->pProx;
+    }
+    return 0;
+}
 /*
     AINDA FALTA:
         - Remove Palavra (dada a palavra)
         - Remove Palavra (do final)
         - Verifica Palavra (dada a palavra, verifica se já existe na lista)
-        - Retorna número de palavras
         - Imprime
 */
