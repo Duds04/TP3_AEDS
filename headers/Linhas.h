4d2f43185245efe1e4e00181f611037ee4634e6a
Arthur-Ataide <114112558+Arthur-Ataide@users.noreply.github.com>
2022-10-10 11:18:50 -0300

    Update Linhas.h

--- headers/Linhas.h
+++ headers/Linhas.h
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Celula_Linha* pLinha; 
-typedef struct Celula_Linha{
-    char Linha; //  char que representa a Linha da palavra
-    struct Celula_Linha* pProx; // apontador pra prox Celula
+typedef struct CelulaLinha* pTipoLinha; 
+typedef struct CelulaLinha{
+    int Linha; //  int que representa as linhas da palavra
+    struct CelulaLinha* pProx; // apontador pra prox linha q tem essa palavra
 }TCelulaLinha;
 
 typedef struct {
-    pLinha pPrimeiro;
-    pLinha pUltimo;
+    pTipoLinha pPrimeiro;
+    pTipoLinha pUltimo;
 }TLinhas;
 
-void LinhaVazia(TLinhas* plista);             // Cria uma nova linha
+void LinhaVazia(TLinhas* pLista);             // Cria uma nova linha
 void InsereLinha(TLinhas* pLista, int pItem);    // Coloca uma nova linha na palavra
 TLinhas RetornarLinha(TLinhas pLista);          // return cadeia inteira(palavra)
 void ImprimeLinhas(TLinhas* pLista);
